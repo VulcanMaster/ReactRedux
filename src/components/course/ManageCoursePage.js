5866fc7b8e5167e0b27e2ac90e97494aed5c126d
VulcanMaster <dmitry.blinkov@outlook.com>
2018-04-06 15:15:42 +0300

    By click on course in course page will be redirect to the manage course page with filling in the selected course into the form of course manage.

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -97,15 +97,23 @@ ManageCoursePage.contextTypes = {
   router: PropTypes.object
 };
 
-// function getCourseById(courses, id) {
-//   const course = courses.filter(course => course.id == id);
-//   if (course) return course[0]; //since filter returns an array, have to grab the first.
-//   return null;
-// }
+function getCourseById(courses, id) {
+  debugger
+  const course = courses.filter(course => course.id == id);
+  if (course) return course[0]; //since filter returns an array, have to grab the first.
+  return null;
+}
 
 function mapStateToProps(state, ownProps) {
+    debugger
+    const courseId = ownProps.params.id; // from the path `/course/:id`
+
     let course = { id: '', watchHref: '', authorId: '', length: '', category: '' }
     
+      if (courseId && state.courses.length > 0) {
+        course = getCourseById(state.courses, courseId);
+      }
+
     const authorsFormattedForDropdown = state.authors.map( author => {
         return {
             value: author.id,
